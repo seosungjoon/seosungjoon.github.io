7fd33b1a9a7c07bc294cc99e684d1239d411b052
seosungjoon <seojj95@naver.com>
2024-05-01 23:44:43 +0900

    [수정] BrowserRouter 새로고침 404 조치

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+const Page404 = React.lazy(() => import('./views/pages/page404/404.html'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 class App extends Component {
