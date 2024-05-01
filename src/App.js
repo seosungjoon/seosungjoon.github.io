fd349e28002f838766c72676232bc2ca3778a8ec
seosungjoon <seojj95@naver.com>
2024-05-02 00:16:09 +0900

    [임시롤백] 404조치 계속

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename="/dashboard">
+      <BrowserRouter /*basename={process.env.PUBLIC_URL}*/>
         <ScrollToTop/>
         <Suspense fallback={loading}>
           <Routes>
             <Route exact path="/login" name="Login Page" element={<Login />} />
             <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
+            {/*<Route exact path="/404" name="Page 404" element={<Page404 />} />*/}
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
             <Route path="*" name="Home" element={<DefaultLayout />} />
           </Routes>
